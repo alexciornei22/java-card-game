68d645f27ed5e49b2a085769c6d5ab5caf537fdd
Alex Ciornei <alexstefciornei22@gmail.com>
2022-11-24 18:46:44 +0200

    add README

--- src/main/Main.java
+++ src/main/Main.java
@@ -73,7 +73,6 @@ public final class Main {
 
         ArrayNode output = objectMapper.createArrayNode();
 
-        //TODO add here the entry point to your implementation
         Player player1 = new Player(inputData.getPlayerOneDecks());
         Player player2 = new Player(inputData.getPlayerTwoDecks());
 
